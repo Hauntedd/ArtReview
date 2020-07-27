90f9617b4ea632e6588c927884f2caeb0220f71b
NealGuo01 <jdguo@uestc.edu.cn>
2020-07-27 20:47:40 +0800

    0727

--- src/main/java/learning/niukeOffer/_04_erweishuzu.java
+++ src/main/java/learning/niukeOffer/_04_erweishuzu.java
@@ -4,13 +4,13 @@ package main.java.learning.niukeOffer;
 在一个 n * m 的二维数组中，每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
 * */
 public class _04_erweishuzu {
-    public boolean findNumberIn2DArray(int[][] matrix, int target) {//从右上角开始查找，逐步排除
+    public boolean findNumberIn2DArray(int[][] matrix, int target) {//从右上角开始查找，逐步排除(线性查找？)
         if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
             return false;
         }
         int rows = matrix.length, columns = matrix[0].length;
         int row = 0, column = columns - 1;
-        while (row < rows && column >= 0) {
+        while (row < rows && column >= 0) {//右上角开始的原因是可以区分两轴的前进方向
             int num = matrix[row][column];
             if (num == target) {
                 return true;
